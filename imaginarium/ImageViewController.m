e15ade3b6e43203d7e4b5aa77a2add2609b0cfc8
XuYeS <xysbit@163.com>
2016-05-22 14:01:21 +0800

    使用AFNetworking

--- imaginarium/ImageViewController.m
+++ imaginarium/ImageViewController.m
@@ -7,7 +7,9 @@
 //
 
 #import "ImageViewController.h"
-
+#import "AFNetworking.h"
+#import "UIKit+AFNetworking.h"
+#import "AFURLSessionManager.h"
 @interface ImageViewController ()<UIScrollViewDelegate>
 @property (nonatomic,strong)UIImageView *imageView;
 @property (nonatomic,strong)UIImage *image;
@@ -40,6 +42,8 @@
     self.image=nil;
     if (self.imageURL)
     {
+/*  use apple api to download
+      
         [self.spinner startAnimating];
         NSURLRequest *requst=[NSURLRequest requestWithURL:self.imageURL];
         NSURLSessionConfiguration *configuration=[NSURLSessionConfiguration ephemeralSessionConfiguration];
@@ -59,6 +63,34 @@
             }
         }];
         [task resume];
+ */
+/*use AFNetworking*/
+        NSURLSessionConfiguration *configuration= [NSURLSessionConfiguration defaultSessionConfiguration];
+        AFURLSessionManager *manager  = [[AFURLSessionManager alloc]initWithSessionConfiguration:configuration];
+        NSURLRequest *request = [NSURLRequest requestWithURL:self.imageURL];
+        
+        NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request
+                                                                        progress:nil
+                                                                     destination:^NSURL * _Nonnull(NSURL * _Nonnull targetPath, NSURLResponse * _Nonnull response) {
+                                                                        
+                                                                         NSString *cacheDir = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)[0];
+                                                                         NSString *path = [cacheDir stringByAppendingPathComponent:response.suggestedFilename];
+                                                                         NSURL *fileURL = [NSURL fileURLWithPath:path];
+                                                                         return fileURL;
+                                                                     }
+                                                                completionHandler:^(NSURLResponse * _Nonnull response, NSURL * _Nullable filePath, NSError * _Nullable error) {
+                                                                   if (!error) {
+                                                                       if ([request.URL isEqual:self.imageURL]) {
+                                                                           UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:filePath]];
+                                                                         dispatch_async(dispatch_get_main_queue(), ^{
+                                                                             self.image = image;
+                                                                         });
+                                                                       }
+                                                                   }
+                                                               }];
+        [downloadTask resume];
+        
+        
     }
     
 }
